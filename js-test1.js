7b37eac30558f9eb4c3b7adf8fd214e20130b66f
Mauma-Awaji <soberemanasseh@gmail.com>
2024-04-28 00:34:32 -0600

    started testing basic Javascript

---
+++ js-test1.js
@@ -0,0 +1,7 @@
+const ashApples = 4;
+const garyApples = 2;
+let total = ashApples + garyApples;
+
+document.getElementById("ashAppleElement").textContent = ashApples;
+document.getElementById("garyAppleElement").textContent = garyApples;
+document.getElementById("totalApplesElement").textContent = total;
